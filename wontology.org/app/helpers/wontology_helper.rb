bcc6f62fba235476dcd714a3d0ed31b5cc30b3d6
Glen E. Ivey <gleneivey@wontology.org>
2010-02-13 08:40:55 -0800

    Home-page customizations:  fill in the bottom-right content column with a hierarchical list of the major (near top of hierarchy) nodes in the Programming Language dataset deployed on wontology.org.

---
+++ wontology.org/app/helpers/wontology_helper.rb
@@ -0,0 +1,34 @@
+# WontoMedia - a wontology web application
+# Copyright (C) 2010 - Glen E. Ivey
+#    www.wontology.com
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version
+# 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program in the file COPYING and/or LICENSE.  If not,
+# see <http://www.gnu.org/licenses/>.
+
+
+module WontologyHelper
+  def link_to_node_by_name(name)
+    unless defined? @nodes_by_name
+      init_name_to_node_hash
+    end
+    n = @nodes_by_name[name]
+    return link_to n.title, node_path(n)
+  end
+
+  def init_name_to_node_hash
+    @nodes_by_name = {}
+    @nouns.each do |node|
+      @nodes_by_name[node.name] = node
+    end
+  end
+end
