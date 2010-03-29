679a0bc6e5f5ddd5c5473ade2da3fa068e4bd280
Glen E. Ivey <gleneivey@wontology.org>
2010-03-29 04:37:44 -0700

    Tweaked deployment scripts while putting WM v1.0.1 live on A2 Hosting.

---
+++ Deploy/wontomedia-on-a2hosting/configs-to-a2.sh
@@ -0,0 +1,6 @@
+### for deploying adasdaughters.org
+scp -P 7822 -r SiteConfigs/ad.*    SiteConfigs/adasdaughters.org    \
+        glenivey@www.wontology.org:/home/glenivey/SiteConfigs
+### for deploying wontology.org
+scp -P 7822 -r SiteConfigs/wm.*    SiteConfigs/wontology.org        \
+        glenivey@www.wontology.org:/home/glenivey/SiteConfigs
--- Deploy/wontomedia-on-a2hosting/deploy-to-a2.sh
+++ Deploy/wontomedia-on-a2hosting/deploy-to-a2.sh
@@ -1,13 +1,3 @@
-
-scp -P 7822 deploy/wontology-on-a2hosting/install-wontomedia.rb    \
-            pkg/wontomedia-*.gem                                   \
-                glenivey@www.wontology.org:/home/glenivey
-# still valid, if commented out, just because we don't need to do every time:
-### for deploying adasdaughters.org
-#scp -P 7822 -r ../wontology-website-configs/SiteConfigs/ad.*           \
-#            ../wontology-website-configs/SiteConfigs/adasdaughters.org \
-#                glenivey@www.wontology.org:/home/glenivey/SiteConfigs
-### for deploying wontology.org
-#scp -P 7822 -r ../wontology-website-configs/SiteConfigs/wm.*           \
-#            ../wontology-website-configs/SiteConfigs/wontology.org \
-#                glenivey@www.wontology.org:/home/glenivey/SiteConfigs
+scp -P 7822    \
+ ~/Repos/Mainlines/wontology-website-configs/Deploy/wontology-on-a2hosting/install-wontomedia.rb     pkg/wontomedia-*.gem                                   \
+        glenivey@www.wontology.org:/home/glenivey
