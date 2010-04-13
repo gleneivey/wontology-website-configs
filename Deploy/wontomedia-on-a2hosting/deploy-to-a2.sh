5eeac5202c1f742567553c8c11c94c110422d0c0
Glen E. Ivey <gleneivey@wontology.org>
2010-04-13 09:56:04 -0700

    Fixing type-o in path.

--- Deploy/wontomedia-on-a2hosting/deploy-to-a2.sh
+++ Deploy/wontomedia-on-a2hosting/deploy-to-a2.sh
@@ -1,3 +1,3 @@
 scp -P 7822    \
- ~/Repos/Mainlines/wontology-website-configs/Deploy/wontology-on-a2hosting/install-wontomedia.rb     pkg/wontomedia-*.gem                                   \
+ ~/Repos/Mainlines/wontology-website-configs/Deploy/wontomedia-on-a2hosting/install-wontomedia.rb     pkg/wontomedia-*.gem                                   \
         glenivey@www.wontology.org:/home/glenivey
