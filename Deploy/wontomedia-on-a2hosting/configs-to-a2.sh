d2e387db6a54870a539a1470c59b5a55c4d51f18
Glen E. Ivey <gleneivey@wontology.org>
2011-03-27 21:09:10 -0700

    Moving assets into per-site repositories

--- Deploy/wontomedia-on-a2hosting/configs-to-a2.sh
+++
@@ -1,6 +0,0 @@
-### for deploying adasdaughters.org
-scp -P 7822 -r SiteConfigs/ad.*    SiteConfigs/adasdaughters.org    \
-        glenivey@www.wontology.org:/home/glenivey/SiteConfigs
-### for deploying wontology.org
-scp -P 7822 -r SiteConfigs/wm.*    SiteConfigs/wontology.org        \
-        glenivey@www.wontology.org:/home/glenivey/SiteConfigs
--- Deploy/wontomedia-on-a2hosting/deploy-to-a2.sh
+++
@@ -1,3 +0,0 @@
-scp -P 7822    \
- ~/Repos/Mainlines/wontology-website-configs/Deploy/wontomedia-on-a2hosting/install-wontomedia.rb     pkg/wontomedia-*.gem                                   \
-        glenivey@www.wontology.org:/home/glenivey
